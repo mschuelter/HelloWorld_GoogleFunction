460499d3e509b249752d479c7025e452543d61b6
Marcos A Schuelter <marcos@habloapp.com>
2020-10-20 11:55:05 -0300

    Testing Cloud Build

---
+++ index.js
@@ -0,0 +1,90 @@
+//gcloud functions deploy helloHttp --runtime nodejs12 --trigger-http --allow-unauthenticated
+
+const escapeHtml = require('escape-html');
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloHttpSimple = (req, res) => {
+    res.send(
+        `<h3>Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!<h3>`
+        + `<h1>Env Variable: ${escapeHtml(process.env.ENV_KEY)}</h1>`
+    );
+}
+
+/**
+ * Responds to a GET request with "Hello World!". Forbids a PUT request.
+ *
+ * @example
+ * gcloud functions call helloHttp
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloHttp = (req, res) => {
+    switch (req.method) {
+      case 'GET':
+        res.status(200).send('Hello World!');
+        break;
+      case 'PUT':
+        res.status(403).send('Forbidden!');
+        break;
+      default:
+        res.status(405).send({error: 'Something blew up!'});
+        break;
+    }
+  };
+
+/**
+ * Responds to an HTTP request using data from the request body parsed according
+ * to the "content-type" header.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloContent = (req, res) => {
+    let name;
+  
+    switch (req.get('content-type')) {
+      // '{"name":"John"}'
+      case 'application/json':
+        ({name} = req.body);
+        break;
+  
+      // 'John', stored in a Buffer
+      case 'application/octet-stream':
+        name = req.body.toString(); // Convert buffer to a string
+        break;
+  
+      // 'John'
+      case 'text/plain':
+        name = req.body;
+        break;
+  
+      // 'name=John' in the body of a POST request (not the URL)
+      case 'application/x-www-form-urlencoded':
+        ({name} = req.body);
+        break;
+    }
+  
+    res.status(200).send(`Hello ${escapeHtml(name || 'World')}!`);
+  };
+
+/**
+ * HTTP Cloud Function.
+ * This function is exported by index.js, and is executed when
+ * you make an HTTP request to the deployed function's endpoint.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloGet = (req, res) => {
+    res.status(200).send('Hello GET World!');
+  };
---
+++ test/index.test.js
@@ -0,0 +1,185 @@
+const proxyquire = require('proxyquire').noCallThru();
+const sinon = require('sinon');
+const assert = require('assert');
+
+const getSample = () => {
+  const requestPromise = sinon
+    .stub()
+    .returns(new Promise((resolve) => resolve('test')));
+
+  return {
+    sample: proxyquire('../', {
+      'request-promise': requestPromise,
+    }),
+    mocks: {
+      requestPromise: requestPromise,
+    },
+  };
+};
+
+const getMocks = () => {
+  const req = {
+    headers: {},
+    get: function (header) {
+      return this.headers[header];
+    },
+  };
+  sinon.spy(req, 'get');
+
+  return {
+    req: req,
+    res: {
+      set: sinon.stub().returnsThis(),
+      send: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+      end: sinon.stub().returnsThis(),
+      status: sinon.stub().returnsThis(),
+    },
+  };
+};
+
+const stubConsole = function () {
+  sinon.stub(console, `error`);
+};
+
+const restoreConsole = function () {
+  console.error.restore();
+};
+
+beforeEach(stubConsole);
+afterEach(restoreConsole);
+
+describe('functions_http_get', () => {
+    it('http:helloGet: should handle GET', () => {
+      const mocks = getMocks();
+      const httpSample = getSample();
+      mocks.req.method = 'GET';
+      httpSample.sample.helloGet(mocks.req, mocks.res);
+  
+      assert.strictEqual(mocks.res.status.calledOnce, true);
+      assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+      assert.strictEqual(mocks.res.send.calledOnce, true);
+      assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello GET World!');
+    });
+});
+
+describe('functions_http_method', () => {
+  it('http:helloHttp: should handle GET', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.method = 'GET';
+    httpSample.sample.helloHttp(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello World!');
+  });
+
+  it('http:helloHttp: should handle PUT', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.method = 'PUT';
+    httpSample.sample.helloHttp(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 403);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Forbidden!');
+  });
+
+  it('http:helloHttp: should handle other methods', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.method = 'POST';
+    httpSample.sample.helloHttp(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 405);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.deepStrictEqual(mocks.res.send.firstCall.args[0], {
+      error: 'Something blew up!',
+    });
+  });
+});
+
+describe('functions_http_content', () => {
+  it('http:helloContent: should handle application/json', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.headers['content-type'] = 'application/json';
+    mocks.req.body = {name: 'John'};
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello John!');
+  });
+
+  it('http:helloContent: should handle application/octet-stream', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.headers['content-type'] = 'application/octet-stream';
+    mocks.req.body = Buffer.from('John');
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello John!');
+  });
+
+  it('http:helloContent: should handle text/plain', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.headers['content-type'] = 'text/plain';
+    mocks.req.body = 'John';
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello John!');
+  });
+
+  it('http:helloContent: should handle application/x-www-form-urlencoded', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.headers['content-type'] = 'application/x-www-form-urlencoded';
+    mocks.req.body = {name: 'John'};
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello John!');
+  });
+
+  it('http:helloContent: should handle other', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(mocks.res.send.firstCall.args[0], 'Hello World!');
+  });
+
+  it('http:helloContent: should escape XSS', () => {
+    const mocks = getMocks();
+    const httpSample = getSample();
+    mocks.req.headers['content-type'] = 'text/plain';
+    mocks.req.body = {name: '<script>alert(1)</script>'};
+    httpSample.sample.helloContent(mocks.req, mocks.res);
+
+    assert.strictEqual(mocks.res.status.calledOnce, true);
+    assert.strictEqual(mocks.res.status.firstCall.args[0], 200);
+    assert.strictEqual(mocks.res.send.calledOnce, true);
+    assert.strictEqual(
+      mocks.res.send.firstCall.args[0].includes('<script>'),
+      false
+    );
+  });
+});
